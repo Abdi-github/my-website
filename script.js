6eff3e519e2f30378084a504a656654e856a1481
abdigit <abdulkadirahmed1985@gmail.com>
2020-08-20 04:16:19 +0200

    final

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-icon').addEventListener('click',showNav);
+
+    function showNav () {
+
+        document.querySelector('.nav-links').classList.toggle('show-links');
+        
+    }
